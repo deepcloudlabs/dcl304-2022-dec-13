beb67c351c4971229f44f3a24bb6395e92faf866
deepcloudlabs <deepcloudlabs@gmail.com>
2022-12-14 15:11:10 +0300

    Update game.js

--- number-game/game.js
+++ number-game/game.js
@@ -8,12 +8,25 @@ class GameViewModel {
         this.updateView = updateView;
         setInterval(this.countDown, 1_000);
     }
-
+    initGame = (wins=false) => {
+        let message = "You have lost the game!";
+        if (wins)
+            message = "You have won the game!";
+        let move = new Move(this.secret, message);
+        this.guess = 50;
+        this.secret= this.createSecret();
+        this.numberOfMoves = 0;
+        this.counter = 60;
+        this.moves = [];
+        this.moves.push(move);
+        this.updateView({
+            counter: this.counter, moves: this.moves, numberOfMoves: this.numberOfMoves})
+    }
     countDown = () => {
         this.counter--;
         this.updateView({counter: this.counter});
         if (this.counter <= 0){
-            //TODO: Player loses
+            this.initGame(false);
         }
     }
     createSecret = () => {
@@ -27,10 +40,10 @@ class GameViewModel {
     play = () => {
         this.numberOfMoves++;
         if (this.guess === this.secret){
-            //TODO: Player wins!
+            this.initGame(true);
         } else {
           if (this.numberOfMoves >= 7){
-            //TODO: Player loses!
+            this.initGame(false);
           } else {
             this.moves.push(new Move(this.guess,this.createMessage()));
             this.updateView({numberOfMoves: this.numberOfMoves, moves: this.moves});
