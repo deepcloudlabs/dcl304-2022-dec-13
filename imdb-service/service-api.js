26687dffb50230fd6e9b31ec215ab9f7f69b4b84
deepcloudlabs <deepcloudlabs@gmail.com>
2022-12-15 08:54:44 +0300

    initial commit

---
+++ imdb-service/service-api.js
@@ -0,0 +1,27 @@
+// How to Write Node Module, ES Module
+// Node Architecture
+
+//region REST API (express.js)
+
+//endregion
+
+//region REST API DOCUMENTATION (swagger-ui, OpenAPI)
+
+//endregion
+
+//region MONGODB INTEGRATION (mongoose |-> mongodb driver)
+
+//endregion
+
+//region KAFKA INTEGRATION (kafkajs)
+
+//endregion ()
+
+//region WEBSOCKET (ws)
+
+//endregion
+
+//region WEBSOCKET (SOCKET-IO)
+
+//endregion
+
